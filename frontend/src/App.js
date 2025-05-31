4120d06eab5bec4ea3a80778d41e5eae64919705
PrinceQuok <artemverbovsky793@gmail.com>
2025-05-31 05:52:26 +0300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-
 function App() {
   const [records, setRecords] = useState([]);
   const [user, setUser] = useState(null);
